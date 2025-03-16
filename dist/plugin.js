0fb6ce03a3fcbf2724c2bd1915f4eae5f5e3aa9a
Massimo Melina <a@rejetto.com>
2025-03-16 15:19:47 +0100

    first commit

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,10 +1,10 @@
 exports.description = "Create links to download a file without login"
 exports.version = 9
+exports.apiRequired = 12.1 // fix on ctx.stop()
 exports.repo = "rejetto/hfs-share-links"
 exports.frontend_js = "main.js"
 exports.config = {
     onlyFor: {
-        frontend: true,
         type: 'username',
         multiple: true,
         label: "Show only for",
@@ -13,59 +13,82 @@ exports.config = {
     links: {
         type: 'array',
         defaultValue: [],
-        fields: {
-            uri: { disabled: true, $width: 1 },
-            token: { disabled: true, $hideUnder: 1e9 },
-            days: { type: 'number', unit: 'days', min: .01, step: .01, defaultValue: 1 },
-            daysStartOnAccess: { type: 'boolean' },
-            expiration: { type: 'date_time' },
-        }
+        form: { maxWidth: 'sm' },
+        fields: values => ({
+            uri: { $width: 1.5, label: "URI", type: 'vfs_path', $mergeRender: { expiration: {} } },
+            token: { required: true, $hideUnder: true },
+            expiration: { type: 'date_time', label: "Expiration date", sm: 5, $hideUnder: 'sm', $width: 1,
+                $render({ value, row }) { return value ? new Date(value).toLocaleString() : `Expires in ${row.days} day(s) after first access` }
+            },
+            days: !values.expiration && { $hideUnder: true, type: 'number', label: "Expire after", unit: 'days', min: 0, step: .01, defaultValue: 1, xs: 5, sm: 3 },
+            daysStartOnAccess: !values.expiration && { $hideUnder: true, type: 'boolean', xs: 7, sm: 4 },
+        })
     },
 }
 exports.configDialog = {
-    maxWidth: '40em'
+    maxWidth: '50em'
 }
 
 exports.init = api => {
     const { getBaseUrlOrDefault } = api.require('./listen')
     const { urlToNode } = api.require('./vfs')
     const { serveFileNode } = api.require('./serveFile')
+    const { _ } = api
     // keep in memory
     let links = []
-    api.subscribeConfig('links', v => links = v)
+    api.subscribeConfig('links', v => links = v.map(x => ({ ...x, expiration: x.expiration && new Date(x.expiration) })))
     // purge
     api.setInterval(() => {
         const now = new Date
-        api.setConfig('links', links.filter(l => !(new Date(l.expiration) < now)))
+        api.setConfig('links', links.filter(l => !(now > new Date(l.expiration))))
     }, 60_000)
 
     return {
         customRest: {
-            async link(values) {
-                const token = api.misc.randomId(50) // 258 bits of randomness
+            async link(values, ctx) {
+                limitAccess(ctx)
+                const token = api.misc.randomId(25) // 128 bits of randomness
                 api.setConfig('links', [...links, { token, ...values }])
                 return { token, baseUrl: await getBaseUrlOrDefault() }
             },
-            async get_links(filter) {
+            async get_links(filter, ctx) {
+                try { limitAccess(ctx) }
+                catch { return false }
                 return {
                     baseUrl: await getBaseUrlOrDefault(),
-                    list: api._.filter(links, filter)
+                    list: _.filter(links, filter)
                 }
             },
-            del_link(filter) {
-                api._.remove(links, filter)
+            del_link(filter, ctx) {
+                limitAccess(ctx)
+                _.remove(links, filter)
                 api.setConfig('links', links)
             },
         },
         async middleware(ctx) {
-            const {sharelink} = ctx.query
+            const { sharelink } = ctx.query
             if (!sharelink) return
+            ctx.stop()
             const link = links.find(l => l.token === sharelink)
-            const valid = link && !(new Date(link.expiration) < new Date)
-            const node = valid && await urlToNode(link.uri)
-            if (!node) return ctx.status = 404
+            if (!link || link.expiration && link.expiration < new Date) return
+            const node = await urlToNode(link.uri)
+            if (!node) return
             await serveFileNode(ctx, node)
-            return ctx.stop()
+            if (!link.daysStartOnAccess || link.expiration) return
+            if (link.days) {
+                link.expiration = new Date(Date.now() + link.days * 86400_000)
+                links = links.slice()
+            }
+            else
+                links = _.without(links, link)
+            api.setConfig('links', links)
         },
+    }
+
+    function limitAccess(ctx) {
+        const limitTo = api.getConfig('onlyFor')
+        if (!limitTo?.length) return
+        if (!api.ctxBelongsTo(ctx, limitTo))
+            throw 401
     }
 }
--- dist/public/main.js
+++ dist/public/main.js
@@ -1,49 +1,65 @@
 'use strict';{
-    const { onlyFor } = HFS.getPluginConfig()
-    const { h, state } = HFS
+    const { h, t, _ } = HFS
+    let data
+    HFS.watchState('username', async () => {
+        data = await HFS.customRestCall('get_links')
+    }, true)
+
+    HFS.onEvent('additionalEntryDetails', ({ entry }) =>
+        _.find(data?.list, { uri: entry.uri }) && HFS.hIcon('link', { title: t('Share-link') }))
 
     HFS.onEvent('fileMenu', ({ entry }) =>
-        (!onlyFor?.length || onlyFor.some(HFS.userBelongsTo))
-        && !entry.isFolder
-        && [{ id: 'share-link', icon: 'link', label: HFS.t("Share-link"), async onClick() {
-            const res = await HFS.customRestCall('get_links', { uri: entry.uri })
+        data && !entry.isFolder
+        && [{ id: 'share-link', icon: 'link', label: t("Share-link"), async onClick() {
+            const ofThisFile = _.filter(data.list, { uri: entry.uri })
             const { close } = await HFS.dialogLib.newDialog({
-                title: HFS.t("Share-link"),
+                title: t("Share-link"),
                 Content() {
-                    const [list, setList] = HFS.React.useState(res.list)
+                    const [list, setList] = HFS.React.useState(ofThisFile)
                     return h('div', {},
                         h('form', {
                             style: { display: 'flex', flexDirection: 'column', gap: '1em' },
                             async onSubmit(ev) {
+                                ev.preventDefault()
                                 const data = new FormData(ev.target)
-                                const res = await HFS.customRestCall('link', {
+                                const days = Number(data.get('days'))
+                                const onAccess = data.get('daysStartOnAccess')
+                                if (!onAccess && !days)
+                                    return HFS.dialogLib.alertDialog(t("0 days makes sense only when the flag is enabled"), 'warning')
+                                HFS.customRestCall('link', {
                                     uri: entry.uri,
-                                    days: Number(data.get('days')),
-                                    daysStartOnAccess: Boolean(data.get('daysStartOnAccess')),
-                                })
-                                close()
-                                copy(res.baseUrl, res.token)
+                                    ...onAccess ? { days, daysStartOnAccess: true }
+                                        : { expiration: new Date(Date.now() + days * 86400_000) },
+
+                                }).then(res => {
+                                    close()
+                                    copy(res.baseUrl, res.token)
+                                }, HFS.dialogLib.alertDialog)
                             },
                         },
+                            t("Link for "), entry.uri,
                             h('div', { className: 'field' },
-                                h('label', {}, HFS.t("Days to live")),
-                                h('input', { type: 'number', name: 'days', min: .01, step: .01, defaultValue: 1 }),
+                                h('label', {}, t("Days to live")),
+                                h('input', { type: 'number', name: 'days', min: 0, step: .01, defaultValue: 1,
+                                    style: { width: '5em', marginLeft: '1em' }
+                                }),
                             ),
-                            h('div', { className: 'field' },
-                                h('label', {}, h('input', { type: 'checkbox', name: 'daysStartOnAccess', value: 1 }), HFS.t("Days start on first access")),
+                            h('label', { className: 'field' },
+                                h('input', { type: 'checkbox', name: 'daysStartOnAccess', value: 1 }),
+                                t("Days start on first access")
                             ),
-                            h('button', { type: 'submit' }, HFS.t("Create share-link")),
+                            h('button', { type: 'submit' }, HFS.hIcon('copy'),' ', t("Create share-link")),
                         ),
-                        list.length > 0 && h('div', { style: { marginTop: '1em', display: 'flex', flexDirection: 'column', gap: '.5em' } },
-                            HFS.t("Existing links on this file"),
+                        list.length > 0 && h('div', { style: { marginTop: '2em', display: 'flex', flexDirection: 'column', gap: '.5em' } },
+                            t("Existing links on this file"),
                             list.map((x, i) => h('div', { key: i },
                                 HFS.iconBtn('delete', () =>
                                     HFS.customRestCall('del_link', { token: x.token })
                                         .then(() => setList(list.filter((_, j) => j !== i)))),
-                                h('button', { onClick() { close(); copy(res.baseUrl, x.token) } },
-                                    HFS.hIcon('copy'), ' ',
-                                    ...x.expiration ? [HFS.t("Expires:"), ' ', HFS.misc.formatTimestamp(x.expiration)]
-                                        : [HFS.t("Days:"), ' ', x.days, ' ', x.daysStartOnAccess && HFS.t("(start on first access)")]
+                                h('button', { onClick() { close(); copy(data.baseUrl, x.token) } },
+                                    HFS.hIcon('copy'), ' ' + t("Copy") + ' - ',
+                                    ...x.expiration ? [t("Expires:"), ' ', HFS.misc.formatTimestamp(x.expiration)]
+                                        : [t("Days:"), ' ', x.days, ' ', x.daysStartOnAccess && t("(start on first access)")]
                                 ),
                             ))
                         ),
@@ -54,6 +70,6 @@
 
     function copy(base, token) {
         HFS.copyTextToClipboard(base + '/?sharelink=' + token)
-        HFS.toast(HFS.t("Share-link copied"), 'success')
+        HFS.toast(t("Share-link copied"), 'success')
     }
 }
